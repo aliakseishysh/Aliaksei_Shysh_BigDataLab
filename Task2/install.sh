4f1f8c7a0b8d6461bab71a1ca4dd1e930c3af272
Aliaksei <abarakabama@gmail.com>
2021-10-01 22:30:31 +0300

    updated install.sh, added git_configure_password.exp and unistall.sh

--- Task2/install.sh
+++ Task2/install.sh
@@ -44,7 +44,7 @@ function tryInstall {
 	else
 		echo "  $@ is not installed"
 		echo "  installing $@"
-		if [ "$verbose" = true ]; then
+		if [ "$verbose_flag" = true ]; then
 			sudo yum -y install $@ >/dev/null 2>/dev/null;
 		else
 			sudo yum -y install $@
@@ -54,43 +54,48 @@ function tryInstall {
 }
 
 ####################################################################
-# function to transform long parameters to short
-# and to set shot parameters to variables 
+# function to set parameters to variables 
 ####################################################################
-function setParameters {
-	# transform long parameters to short
-	for arg in "$@"; do
-		shift
-		case "$arg" in
-			"--help") set -- "$@" "-h";;
-			"--verbose") set -- "$@" "-v";;
-		        "--git-ssh-password") set -- "$@" "-g";;	
-			*) set -- "$@" "$arg"
-		esac
-	done
+#die() { echo "$*" >&2; exit 2; }
+#needs_arg() { if [ -z "$OPTARG" ]; then die "No arg for --$OPT option"; fi; }
 
-	verbose=false;
-	help_message=false;
+function setParameters() {
+	die() { echo "$*" >&2; exit 2; }
+	needs_arg() { if [ -z "$1" ]; then die "No arg for --$OPT option"; fi; }
 
-	# set short parametes to variables
-	while getopts vhg: flag
-	do
-		case "${flag}" 	in
-			v) verbose=true;;
-			h) help_message=true;;
-			g) git_ssh_password="${OPTARG}";; 
+	OPTIND=1
+	help_flag=false
+	verbose_flag=false
+	username=""
+	gpg_key_id=""
+	ssh_key_passphrase=""
+	email=""
+	name=""
+	
+	while getopts hvu:g:s:e:n:-: OPT; do
+		if [[ "$OPT" = "-" ]]; then		# long option: reformulate OPT and OPTARG
+			OPT="${OPTARG%%=*}"		# extract long option name
+			OPTARG="${OPTARG#$OPT}"		# extract long option argument (may be empty)
+			OPTARG="${OPTARG#=}"		# if long option argument, remove assigning `=`
+		fi
+		case "$OPT" in
+			h | help)			help_flag=true ;;
+			v | verbose)			verbose_flag=true ;;
+			u | username)			needs_arg "$OPTARG"; username="$OPTARG" ;;
+			e | email)			needs_arg "$OPTARG"; email="$OPTARG" ;;
+			n | name)			needs_arg "$OPTARG"; name="$OPTARG" ;;
+			g | gpg-key-id)			needs_arg "$OPTARG"; gpg_key_id="$OPTARG" ;;
+			s | ssh-key-passphrase)		needs_arg "$OPTARG"; ssh_key_passphrase="$OPTARG" ;;
+			??*)				die "Illegal option --$OPT" ;;  # bad long option
+			\?)				exit 2 ;;  # bad short option (error reported via getopts)
 		esac
 	done
 }
 
 
-
-
-# script
-
-setParameters;
+setParameters $@;
 # check for help_message
-if $help_message; then
+if $help_flag; then
 	printHelpMessage;
 	exit 0;
 fi
@@ -98,11 +103,46 @@ fi
 
 # git installation
 tryInstall git;
-
-
-
-
+# ssh configuration
+if [[ ! -z "$username" && ! -z "$ssh_key_passphrase" ]]; then
+	echo "Start configuring git ssh"
+	echo "$name"
+	if [ "$verbose_flag" = true ]; then
+		eval `ssh-agent` >/dev/null;
+		./git_configure_password.exp "$username" "$ssh_key_passphrase" >/dev/null;
+	else
+		eval `ssh-agent`
+		./git_configure_password.exp "$username" "$ssh_key_passphrase"
+	fi
+	if [[ ! -z "$email" && ! -z "$name" ]]; then
+		sudo -i -u "$username" git config --global user.email "$email"
+                sudo -i -u "$username" git config --global user.name "$name"
+	fi
+	echo "Git ssh configured"
+fi
+#gpg configuration
+if [[ ! -z "$gpg_key_id" ]]; then
+	# gpg_key=gpg --armor --export "$gpg_key_id"
+	echo "Start configuring git gpg"
+	sudo -i -u "$username" git config --global user.signingkey "$gpg_key_id"
+	sudo -i -u "$username" git config --global commit.gpgsign true
+	echo "Git gpg configured"
+fi
 
 tryInstall java-1.8.0-openjdk;
 tryInstall maven;
-tryInstall postgresql;
+tryInstall postgresql-server;
+# postgresql configuration
+echo "Start configuring postgresql"
+sudo postgresql-setup --initdb
+sudo systemctl enable postgresql.service
+sudo systemctl start postgresql.service
+
+sudo -i -u postgres createuser "${username}" --createdb
+sudo -i -u postgres psql -c "create database ${username} with owner ${username};"
+echo "Postgresql configured"
+
+
+
+
+
---
+++ Task2/uninstall.sh
@@ -0,0 +1,6 @@
+sudo yum -y erase git
+rm -rf /usr/bin/git
+#sudo yum -y erase java
+#sudo yum -y erase maven
+sudo yum -y erase postgresql-server
+rm -rf /var/lib/pgsql
