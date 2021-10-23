6cff7fb5f58d9471801e43006d03ac1404221c9a
Aliaksei A. Shysh <abarakabama@gmail.com>
2021-10-23 19:09:58 +0300

    query 4 update

--- Task4/sql/queries/4.sql
+++ Task4/sql/queries/4.sql
@@ -1,16 +1,26 @@
+WITH cte AS (
+    SELECT
+        stopAndSearches.officer_defined_ethnicity AS cte_ethnicity,
+        stopAndSearches.object_of_search AS cte_obj_of_search,
+        count(*) AS n,
+        ROW_NUMBER() OVER (PARTITION BY stopAndSearches.officer_defined_ethnicity ORDER BY count(*) DESC) AS rn
+    FROM stopAndSearches
+    GROUP BY officer_defined_ethnicity, object_of_search
+)
 SELECT DISTINCT
     stopAndSearches.officer_defined_ethnicity,
     count(stopAndSearches.sas_id) OVER(PARTITION BY stopAndSearches.officer_defined_ethnicity) AS "Sas for ethnicity",
     sum(case when stopAndSearches.outcome = 'Arrest' then 1 else 0 end)
-       over (partition by stopAndSearches.officer_defined_ethnicity) ::float /
-       count(stopAndSearches.sas_id) OVER(PARTITION BY stopAndSearches.officer_defined_ethnicity) ::float * 100 AS "Arrest rate",
-    sum(case when stopAndSearches.outcome = 'A no further action disposal' then 1 else 0 end)
-       over (partition by stopAndSearches.officer_defined_ethnicity) ::float /
-       count(stopAndSearches.sas_id) OVER(PARTITION BY stopAndSearches.officer_defined_ethnicity) ::float * 100 AS "No action rate",
-    sum(case when stopAndSearches.outcome NOT IN ('Arrest', 'A no further action disposal') then 1 else 0 end)
-       over (partition by stopAndSearches.officer_defined_ethnicity) ::float /
-       count(stopAndSearches.sas_id) OVER(PARTITION BY stopAndSearches.officer_defined_ethnicity) ::float * 100 AS "Other outcome rate"
-    -- mode() WITHIN GROUP (ORDER BY stopAndSearches.object_of_search)-- stopAndSearches.object_of_search ) -- don't work
+       OVER (PARTITION BY stopAndSearches.officer_defined_ethnicity) ::float /
+       count(stopAndSearches.sas_id) OVER(PARTITION BY stopAndSearches.officer_defined_ethnicity)::float * 100 AS "Arrest rate",
+   sum(case when stopAndSearches.outcome = 'A no further action disposal' then 1 else 0 end)
+      OVER (PARTITION BY stopAndSearches.officer_defined_ethnicity) ::float /
+      count(stopAndSearches.sas_id) OVER(PARTITION BY stopAndSearches.officer_defined_ethnicity)::float * 100 AS "No action rate",
+   sum(case when stopAndSearches.outcome NOT IN ('Arrest', 'A no further action disposal') then 1 else 0 end)
+      OVER (PARTITION BY stopAndSearches.officer_defined_ethnicity) ::float /
+      count(stopAndSearches.sas_id) OVER(PARTITION BY stopAndSearches.officer_defined_ethnicity)::float * 100 AS "Other outcome rate",
+   cte.cte_obj_of_search AS "Most popular object of search"
 FROM stopAndSearches
-WHERE stopAndSearches.datetime >= :start_date::date AND stopAndSearches.datetime <= :end_date::date
-GROUP BY stopAndSearches.sas_id
+INNER JOIN cte ON stopAndSearches.officer_defined_ethnicity IS NOT DISTINCT FROM cte.cte_ethnicity
+WHERE rn = 1 AND stopAndSearches.datetime >= :start_date AND stopAndSearches.datetime <= :end_date
+ORDER BY stopAndSearches.officer_defined_ethnicity;
